4e33fed1734e4f4966c86ae7f5af067f4ada610c
ppichier <pierantonio.pichierri@gmail.com>
2020-01-01 01:00:12 +0100

    [ADD] boilerplate, user components / core components / config setup

---
+++ src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signin = () => (
+  <Layout title="Signin" description="Signin Node React Ecommerce App">
+    ...
+  </Layout>
+);
+
+export default Signin;
